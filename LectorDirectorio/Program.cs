b71d58763f856d756072ee94763232d5b4fb7fb1
Agustin Romero <agustin.romero.31.7.00@gmail.com>
2026-06-10 21:22:55 -0300

    punto 1 terminado

---
+++ LectorDirectorio/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters;
+
+Console.WriteLine("--------------------------------- Sistema de Solicitud de Datos -----------------------------------");
+string DirectorioIngresado;
+
+do
+{
+    Console.WriteLine("Ingrese el directorio a analizar: ");
+    DirectorioIngresado = Console.ReadLine();
+    if(!Directory.Exists(DirectorioIngresado))
+    {
+        Console.WriteLine("Error. Ingrese un directorio valido.");
+    } else
+    {
+        string[] Carpetas = Directory.GetDirectories(DirectorioIngresado);
+        string[] Archivos = Directory.GetFiles(DirectorioIngresado);
+
+        for(int i = 0; i < Carpetas.Length; i++)
+        {
+            DirectoryInfo Carpeta = new DirectoryInfo(Carpetas[i]);
+            Console.WriteLine(Carpeta.Name);
+        }
+        
+        StreamWriter Reporte = new StreamWriter("reporte_archivos.csv");
+        Reporte.WriteLine("Nombre  |  Kb  |  Fecha Modificacion");
+
+        for(int j = 0; j < Archivos.Length; j++)
+        {
+            FileInfo Archivo = new FileInfo(Archivos[j]);
+            double Kbytes = (double)Archivo.Length / 1024;
+            if (Archivo.Name != "reporte_archivos.csv")
+            {
+                Console.WriteLine($"{Archivo.Name} - {Kbytes:F2} Kb.");
+                Reporte.WriteLine($"{Archivo.Name}  |  {Kbytes:F2} Kb  |  {Archivo.LastWriteTime}");
+            }
+        }
+        Reporte.Close();
+    }
+} while (!Directory.Exists(DirectorioIngresado));
